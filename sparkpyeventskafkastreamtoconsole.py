639aa58a1ed4e3988c951a990d85ec66d93338ae
Ahmed Gharib <a.gharib89@yahoo.com>
2021-12-12 18:00:52 +0200

    Complete sparkpyeventskafkastreamtoconsole.py

--- sparkpyeventskafkastreamtoconsole.py
+++ sparkpyeventskafkastreamtoconsole.py
@@ -7,14 +7,39 @@ from pyspark.sql.types import (
     BooleanType,
     ArrayType,
     DateType,
+    FloatType,
 )
 
-# TO-DO: using the spark application object, read a streaming dataframe from the Kafka topic stedi-events as the source
+KAFKA_BOOTSTRAP_SERVERS = "localhost:9092"
+
+# create a StructType for the Kafka stedi-events topic which has the Customer Risk JSON that comes from Redis
+stediSchema = StructType(
+    [
+        StructField("customer", StringType()),
+        StructField("score", FloatType()),
+        StructField("riskDate", DateType()),
+    ]
+)
+
+# using the spark application object, read a streaming dataframe from the Kafka topic stedi-events as the source
 # Be sure to specify the option that reads all the events from the topic including those that were published before you started the spark stream
+spark = SparkSession.builder.appName("SparkPyEventKafkaStreamToConsole").getOrCreate()
+spark.sparkContext.setLogLevel("WARN")
 
-# TO-DO: cast the value column in the streaming dataframe as a STRING
+stediEventsRawStreamingDF = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS)
+    .option("subscribe", "stedi-events")
+    .option("startingOffsets", "earliest")
+    .load()
+)
 
-# TO-DO: parse the JSON from the single column "value" with a json object in it, like this:
+# cast the value column in the streaming dataframe as a STRING
+stediEventsStreamingDF = stediEventsRawStreamingDF.selectExpr(
+    "CAST(value as STRING) value"
+)
+
+# parse the JSON from the single column "value" with a json object in it, like this:
 # +------------+
 # | value      |
 # +------------+
@@ -29,9 +54,17 @@ from pyspark.sql.types import (
 # +------------+-----+-----------+
 #
 # storing them in a temporary view called CustomerRisk
-# TO-DO: execute a sql statement against a temporary view, selecting the customer and the score from the temporary view, creating a dataframe called customerRiskStreamingDF
-# TO-DO: sink the customerRiskStreamingDF dataframe to the console in append mode
-#
+# execute a sql statement against a temporary view, selecting the customer and the score from the temporary view, creating a dataframe called customerRiskStreamingDF
+# sink the customerRiskStreamingDF dataframe to the console in append mode
+stediEventsStreamingDF = stediEventsStreamingDF.withColumn(
+    "value", from_json(col("value"), stediSchema)
+).select(col("value.*"))
+
+stediEventsStreamingDF.createOrReplaceTempView("CustomerRisk")
+
+stediEventsStreamingDF.writeStream.outputMode("append").format(
+    "console"
+).start().awaitTermination()
 # It should output like this:
 #
 # +--------------------+-----
